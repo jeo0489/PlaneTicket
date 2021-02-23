98e08792e2044e2a0c1fdcb149b48aa74556623e
jeo0489 <jeo0489@hereford.ac.uk>
2021-02-23 13:41:07 +0000

    Form and code. Validation seems to be correct. Needs discount logic and individual cost calculation

--- PlaneTicket/Form1.Designer.cs
+++ PlaneTicket/Form1.Designer.cs
@@ -27,13 +27,320 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.DestinationSelector = new System.Windows.Forms.ComboBox();
+			this.UpDwnAdults = new System.Windows.Forms.NumericUpDown();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.dateTimeDOB = new System.Windows.Forms.DateTimePicker();
+			this.radioExtraBags = new System.Windows.Forms.RadioButton();
+			this.UpDwnExtraBags = new System.Windows.Forms.NumericUpDown();
+			this.dateTimeDeparture = new System.Windows.Forms.DateTimePicker();
+			this.radioReturn = new System.Windows.Forms.RadioButton();
+			this.radioFreqDiscount = new System.Windows.Forms.RadioButton();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.UpDwnChild = new System.Windows.Forms.NumericUpDown();
+			this.UpDwnOAP = new System.Windows.Forms.NumericUpDown();
+			this.lblName = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.lblCost = new System.Windows.Forms.Label();
+			this.btnGetCost = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.UpDwnAdults)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.UpDwnExtraBags)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.UpDwnChild)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.UpDwnOAP)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// DestinationSelector
+			// 
+			this.DestinationSelector.FormattingEnabled = true;
+			this.DestinationSelector.Location = new System.Drawing.Point(118, 48);
+			this.DestinationSelector.Name = "DestinationSelector";
+			this.DestinationSelector.Size = new System.Drawing.Size(121, 21);
+			this.DestinationSelector.TabIndex = 0;
+			// 
+			// UpDwnAdults
+			// 
+			this.UpDwnAdults.Location = new System.Drawing.Point(118, 87);
+			this.UpDwnAdults.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+			this.UpDwnAdults.Name = "UpDwnAdults";
+			this.UpDwnAdults.Size = new System.Drawing.Size(120, 20);
+			this.UpDwnAdults.TabIndex = 1;
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(118, 179);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(100, 20);
+			this.txtName.TabIndex = 2;
+			// 
+			// dateTimeDOB
+			// 
+			this.dateTimeDOB.Location = new System.Drawing.Point(119, 205);
+			this.dateTimeDOB.Name = "dateTimeDOB";
+			this.dateTimeDOB.Size = new System.Drawing.Size(220, 20);
+			this.dateTimeDOB.TabIndex = 3;
+			// 
+			// radioExtraBags
+			// 
+			this.radioExtraBags.AutoSize = true;
+			this.radioExtraBags.Location = new System.Drawing.Point(119, 241);
+			this.radioExtraBags.Name = "radioExtraBags";
+			this.radioExtraBags.Size = new System.Drawing.Size(14, 13);
+			this.radioExtraBags.TabIndex = 4;
+			this.radioExtraBags.TabStop = true;
+			this.radioExtraBags.UseVisualStyleBackColor = true;
+			// 
+			// UpDwnExtraBags
+			// 
+			this.UpDwnExtraBags.Location = new System.Drawing.Point(219, 241);
+			this.UpDwnExtraBags.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+			this.UpDwnExtraBags.Name = "UpDwnExtraBags";
+			this.UpDwnExtraBags.Size = new System.Drawing.Size(120, 20);
+			this.UpDwnExtraBags.TabIndex = 5;
+			// 
+			// dateTimeDeparture
+			// 
+			this.dateTimeDeparture.Location = new System.Drawing.Point(119, 276);
+			this.dateTimeDeparture.Name = "dateTimeDeparture";
+			this.dateTimeDeparture.Size = new System.Drawing.Size(220, 20);
+			this.dateTimeDeparture.TabIndex = 6;
+			// 
+			// radioReturn
+			// 
+			this.radioReturn.AutoSize = true;
+			this.radioReturn.Location = new System.Drawing.Point(118, 303);
+			this.radioReturn.Name = "radioReturn";
+			this.radioReturn.Size = new System.Drawing.Size(57, 17);
+			this.radioReturn.TabIndex = 7;
+			this.radioReturn.TabStop = true;
+			this.radioReturn.Text = "Return";
+			this.radioReturn.UseVisualStyleBackColor = true;
+			// 
+			// radioFreqDiscount
+			// 
+			this.radioFreqDiscount.AutoSize = true;
+			this.radioFreqDiscount.Location = new System.Drawing.Point(119, 327);
+			this.radioFreqDiscount.Name = "radioFreqDiscount";
+			this.radioFreqDiscount.Size = new System.Drawing.Size(137, 17);
+			this.radioFreqDiscount.TabIndex = 8;
+			this.radioFreqDiscount.TabStop = true;
+			this.radioFreqDiscount.Text = "Frequent Flyer Discount";
+			this.radioFreqDiscount.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 48);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(60, 13);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "Destination";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(12, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(345, 15);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "Safe-ish Airlines -- Where your safety is sometimes considered";
+			// 
+			// UpDwnChild
+			// 
+			this.UpDwnChild.Location = new System.Drawing.Point(117, 113);
+			this.UpDwnChild.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+			this.UpDwnChild.Name = "UpDwnChild";
+			this.UpDwnChild.Size = new System.Drawing.Size(120, 20);
+			this.UpDwnChild.TabIndex = 11;
+			// 
+			// UpDwnOAP
+			// 
+			this.UpDwnOAP.Location = new System.Drawing.Point(118, 139);
+			this.UpDwnOAP.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+			this.UpDwnOAP.Name = "UpDwnOAP";
+			this.UpDwnOAP.Size = new System.Drawing.Size(120, 20);
+			this.UpDwnOAP.TabIndex = 12;
+			// 
+			// lblName
+			// 
+			this.lblName.AutoSize = true;
+			this.lblName.Location = new System.Drawing.Point(12, 89);
+			this.lblName.Name = "lblName";
+			this.lblName.Size = new System.Drawing.Size(69, 13);
+			this.lblName.TabIndex = 14;
+			this.lblName.Text = "Adult Tickets";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 115);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(68, 13);
+			this.label3.TabIndex = 15;
+			this.label3.Text = "Child Tickets";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 141);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(67, 13);
+			this.label4.TabIndex = 16;
+			this.label4.Text = "OAP Tickets";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 182);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(54, 13);
+			this.label5.TabIndex = 17;
+			this.label5.Text = "Full Name";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(12, 206);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 13);
+			this.label6.TabIndex = 18;
+			this.label6.Text = "Adult\'s Date of Birth";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(12, 276);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(92, 13);
+			this.label7.TabIndex = 19;
+			this.label7.Text = "Date of Departure";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(12, 241);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(77, 13);
+			this.label8.TabIndex = 20;
+			this.label8.Text = "Extra Baggage";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(163, 243);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(41, 13);
+			this.label9.TabIndex = 21;
+			this.label9.Text = "Weight";
+			// 
+			// lblCost
+			// 
+			this.lblCost.AutoSize = true;
+			this.lblCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblCost.Location = new System.Drawing.Point(204, 371);
+			this.lblCost.Name = "lblCost";
+			this.lblCost.Size = new System.Drawing.Size(33, 15);
+			this.lblCost.TabIndex = 23;
+			this.lblCost.Text = "cost";
+			// 
+			// btnGetCost
+			// 
+			this.btnGetCost.Location = new System.Drawing.Point(103, 368);
+			this.btnGetCost.Name = "btnGetCost";
+			this.btnGetCost.Size = new System.Drawing.Size(75, 23);
+			this.btnGetCost.TabIndex = 24;
+			this.btnGetCost.Text = "Get Cost";
+			this.btnGetCost.UseVisualStyleBackColor = true;
+			this.btnGetCost.Click += new System.EventHandler(this.btnGetCost_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(381, 415);
+			this.Controls.Add(this.btnGetCost);
+			this.Controls.Add(this.lblCost);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.lblName);
+			this.Controls.Add(this.UpDwnOAP);
+			this.Controls.Add(this.UpDwnChild);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.radioFreqDiscount);
+			this.Controls.Add(this.radioReturn);
+			this.Controls.Add(this.dateTimeDeparture);
+			this.Controls.Add(this.UpDwnExtraBags);
+			this.Controls.Add(this.radioExtraBags);
+			this.Controls.Add(this.dateTimeDOB);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.UpDwnAdults);
+			this.Controls.Add(this.DestinationSelector);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.UpDwnAdults)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.UpDwnExtraBags)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.UpDwnChild)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.UpDwnOAP)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ComboBox DestinationSelector;
+		private System.Windows.Forms.NumericUpDown UpDwnAdults;
+		private System.Windows.Forms.TextBox txtName;
+		private System.Windows.Forms.DateTimePicker dateTimeDOB;
+		private System.Windows.Forms.RadioButton radioExtraBags;
+		private System.Windows.Forms.NumericUpDown UpDwnExtraBags;
+		private System.Windows.Forms.DateTimePicker dateTimeDeparture;
+		private System.Windows.Forms.RadioButton radioReturn;
+		private System.Windows.Forms.RadioButton radioFreqDiscount;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.NumericUpDown UpDwnChild;
+		private System.Windows.Forms.NumericUpDown UpDwnOAP;
+		private System.Windows.Forms.Label lblName;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label lblCost;
+		private System.Windows.Forms.Button btnGetCost;
 	}
 }
 
--- PlaneTicket/Form1.cs
+++ PlaneTicket/Form1.cs
@@ -5,16 +5,157 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PlaneTicket
-{
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
+namespace PlaneTicket {
+	public partial class Form1 : Form {
+		private string[] destinations = {
+			"Munich (MUC)",
+			"London Heathrow (LHR)",
+			"Liverpool J. Lennon (LVP)"
+		};
+
+		public Form1() {
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			DestinationSelector.Items.AddRange(destinations);
+			dateTimeDOB.MinDate = new DateTime(1900, 1, 1);
+			dateTimeDOB.MaxDate = new DateTime(2004, 12, 12);
+			dateTimeDeparture.MinDate = DateTime.Today;
+		}
+
+		private bool validLocation() {
+			foreach (string dest in DestinationSelector.Items) {
+				if (dest == DestinationSelector.Text) {
+					return true;
+				}
+			}
+
+			MessageBox.Show("Invalid Airport!");
+			DestinationSelector.Text = "";
+			return false;
+		}
+
+		private bool validTicketNum() {
+			if (UpDwnAdults.Value < 1) {
+				MessageBox.Show("No Adult tickets!");
+				return false;
+			} else if (UpDwnAdults.Value > 50 || UpDwnChild.Value > 50 || UpDwnOAP.Value > 50) {
+				MessageBox.Show("No ticket order > 50!");
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		private bool validName() {
+			if (Regex.IsMatch(txtName.Text, @"^\[0-9]+$")) {
+				MessageBox.Show("Numeric names are not allowed!");
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		private bool validExtraBag() {
+			if (radioExtraBags.Checked && UpDwnExtraBags.Value <= 0) {
+				MessageBox.Show("The extra bagagge amount (in kg) must be > 0!");
+				return false;
+			} else if (radioExtraBags.Checked && UpDwnExtraBags.Value > 25) {
+				MessageBox.Show("The extra bagagge amount (in kg) must be <= 25!");
+				return false;
+			}
+			return true;
+		}
+
+		private bool allValid() {
+			if (validLocation() && validTicketNum() && validName() && validExtraBag()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		int getRate() {
+			switch (DestinationSelector.Text) {
+				case "Munich (MUC)":
+					return 90;
+				case "London Heathrow (LHR)":
+					return 35;
+				case "Liverpool J. Lennon (LVP)":
+					return 55;
+			}
+			return -1;
+		}
+
+		private float discountForX(int X) {
+			// Returns a multiplier for the cost of an individual to be multiplied by
+			float mult = 1f;
+
+			if(X == 1) { // Gets child discount
+				mult = mult - 0.15f;			// 15 percent off
+			} else if (X == 2) { // Gets OAP discount
+				mult = mult = 0.1f;				// 10 percent off
+			}
+			
+			return mult;
+		}
+
+		private float finalDiscount() {
+			// Returns a multiplier for the total cost to be multiplied by
+			/* Global Discounts include:
+			 *		
+			 * 
+			 */
+			float mult = 1f;
+			return mult;
+		}
+
+		private float costForX(int X, int numOfX) {
+			if (X > 2 || X < 0) {
+				Console.WriteLine("invalid value of X - out of range");
+				return -1;
+			}
+
+			// X signifies either Adult (0), Child (1) or OAP (2) - Use enum next time
+			switch (X) {
+				case 0:
+					return (getRate() * discountForX(0)) * numOfX;
+				case 1:
+					return (getRate() * discountForX(1)) * numOfX;
+				case 2:
+					return (getRate() * discountForX(2)) * numOfX;
+			}
+			return -1.0f;
+		}
+
+		private void showCost(float cost) {
+			lblCost.Text = "";
+		}
+
+		void btnGetCost_Click(object sender, EventArgs e) {
+			/* 
+			 * adult's cost at index 0
+			 * children's cost at index 1
+			 * OAP's cost at index 2
+			 */
+			int passengers = (int)UpDwnAdults.Value + (int)UpDwnChild.Value + (int)UpDwnOAP.Value;
+			int numOfAdults = (int)UpDwnAdults.Value, numOfChildren = (int)UpDwnChild.Value, numOfOAP = (int)UpDwnOAP.Value;
+			float totalCost;
+			float[] individualCosts = { 0.0f, 0.0f, 0.0f };
+			individualCosts[0] = costForX(0, numOfAdults);
+			individualCosts[1] = costForX(1, numOfChildren);
+			individualCosts[2] = costForX(2, numOfOAP);
+
+			if (allValid()) {
+
+				totalCost = individualCosts.Sum() * finalDiscount();
+				showCost(totalCost);
+			}
 		}
 	}
 }
